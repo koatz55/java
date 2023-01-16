6f89220f105690d0efa13c56cb6d3eecd3b92b46
MOISE PIERRE-LOUIS <106705081+koatz55@users.noreply.github.com>
2023-01-16 18:46:51 -0500

    Add files via upload

---
+++ fundamentals/Message.java
@@ -0,0 +1,7 @@
+public class Message {
+    public static void main(String[] args){
+        System.out.print("My name is Moise");
+        System.out.print("I am 26 years old");
+        System.out.print("My hometown is fort-Lauderdale, FL");
+    }
+}
